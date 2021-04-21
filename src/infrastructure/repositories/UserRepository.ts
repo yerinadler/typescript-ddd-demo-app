bc1761fd02232641cbe51d5e20f893d4817a7369
Chatthana Janethanakarn <chatthana@rentspree.com>
2021-04-21 14:42:20 +0700

    Revise injectability

--- src/infrastructure/repositories/UserRepository.ts
+++ src/infrastructure/repositories/UserRepository.ts
@@ -2,10 +2,11 @@ import { TYPES } from '@constants/types';
 import { IDataMapper } from '@core/IDataMapper';
 import { IUserRepository } from '@domain/user/IUserRepository';
 import { User } from '@domain/user/User';
-import { inject } from 'inversify';
+import { inject, injectable } from 'inversify';
 import { Db } from 'mongodb';
 import { Repository } from './Repository';
 
+@injectable()
 export class UserRepository extends Repository<User> implements IUserRepository {
   constructor(
     @inject(TYPES.Db) private readonly db: Db,
